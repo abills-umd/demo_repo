ca553f565ffc7f7e60195d062083e68208119ce0
Aric Bills <abills@umd.edu>
2022-03-30 14:20:00 -0400

    Edit docstring of Person class

--- family.py
+++ family.py
@@ -5,6 +5,7 @@ class Person:
         name (str): the person's name.
         age (int): the person's age.
         gender (str): the person's gender.
+        spouse (Person or None): who the person is married to (if anyone).
     """
     def __init__(self, name, age, gender):
         self.name = name
